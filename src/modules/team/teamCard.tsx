2ca149b960d8962500a845fa58f128dc68ea3e62
alnaki <alexandre.nieto12@gmail.com>
2019-10-09 23:03:39 +0200

    Nouvelle form des tickets + Dialog

--- src/modules/team/teamCard.tsx
+++ src/modules/team/teamCard.tsx
@@ -3,14 +3,13 @@ import {
   Avatar,
   CardHeader,
   IconButton,
-  Dialog,
-  DialogActions,
-  Button
+  Grid,
+  Divider,
+  Typography,
 } from "@material-ui/core";
 import { styled } from "@material-ui/core/styles";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { ITeam } from "../../store/team/types";
-import TeamForm from "./teamForm";
 
 type Props = {
   team: ITeam;
@@ -45,51 +44,46 @@ export default class TeamCard extends Component<Props, State> {
     switch (this.props.variant) {
       case "small":
         return (
-          <CardHeader
-            title={this.props.team.name}
-            action={2}
-            disableTypography
-          />
+          <Grid container wrap="nowrap" spacing={1}>
+            <Grid item>
+              <Divider orientation="vertical" />
+            </Grid>
+            <Grid item zeroMinWidth>
+              <Typography noWrap>{this.props.team.name} d,kadjfdkflzeoo z lfz lq</Typography>
+            </Grid>
+            <Grid item>
+              <Divider orientation="vertical" />
+            </Grid>
+            <Grid item>
+              <Typography>1 </Typography>
+            </Grid>
+          </Grid>
         );
       case "medium":
         return (
           <div>
-            <CardHeader
-              avatar={
-                this.props.team.avatar ? (
-                  <Avatar src={this.props.team.avatar} />
-                ) : (
-                  <MyAvatar>{this.props.team.name.substr(0, 2)}</MyAvatar>
-                )
-              }
-              action={
-                <IconButton
-                  aria-label="settings"
-                  onClick={this.handleClickOpen}
-                >
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title={this.props.team.name}
-              subheader=""
-            />
+            <Grid container wrap="nowrap" spacing={1}>
+              <Grid item>
 
-            <Dialog
-              open={this.state.open}
-              onClose={this.handleClose}
-              aria-labelledby="form-dialog-title"
-            >
-              <TeamForm name={this.props.team.name} />
-              <DialogActions>
-                <Button onClick={this.handleClose} color="primary">
-                  Cancel
-                </Button>
-                <Button onClick={this.handleClose} color="primary">
-                  Subscribe
-                </Button>
-              </DialogActions>
-            </Dialog>
-          </div>
+                {
+                  this.props.team.avatar ? (
+                    <Avatar src={this.props.team.avatar} />
+                  ) : (
+                      <MyAvatar>{this.props.team.name.substr(0, 2)}</MyAvatar>
+                    )
+                }
+              </Grid>
+              <Grid item>
+                <Divider orientation="vertical" />
+              </Grid>
+              <Grid item zeroMinWidth>
+                <Typography noWrap>{this.props.team.name} d,kadjfdkflzeoo z lfz lq</Typography>
+              </Grid>
+              <Grid item>
+                <Divider orientation="vertical" />
+              </Grid>
+            </Grid>
+          </div >
         );
     }
   }
